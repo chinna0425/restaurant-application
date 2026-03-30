82e3c4cc8de0332d18e2de081c7789290fb74158
chinna0425 <chinnakandula2001@gmail.com>
2026-03-30 11:59:32 +0530

    updated not found page

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./index.css";
+import { Button } from "@mui/material";
 
 const NotFound = () => {
 	const role = localStorage.getItem("role")?.trim();
@@ -10,15 +11,18 @@ const NotFound = () => {
 				alt="not-found"
 				loading="lazy"
 			/>
-			<h1 className="not-found-no-orders-title">Page Not Found</h1>
-			<p className="not-found-empty-para">
+			<h1 className="text-2xl mt-3">Page Not Found</h1>
+			<p className="text-xl mt-2">
 				We are sorry,the page you requested could not be found.
 			</p>
-			<p className="not-found-empty-para">Please go back to the homepage.</p>
-			<Link to={`${role === "ROLE_CUSTOMER" ? "/" : "/admin/restaurants"}`}>
-				<button type="button" className="not-found-place-order-style">
+			<p className="text-xl mt-3">Please go back to the homepage.</p>
+			<Link
+				to={`${role === "ROLE_CUSTOMER" ? "/" : "/admin/restaurants"}`}
+				className="mt-5"
+			>
+				<Button variant="contained" type="button">
 					Home Page
-				</button>
+				</Button>
 			</Link>
 		</div>
 	);
