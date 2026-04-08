d4dd058f2de2a3f195ee48c9817b624e2ff96d0e
chinna0425 <chinnakandula2001@gmail.com>
2026-04-08 21:26:50 +0530

    added wake up api

--- OnlineFoodOrdering/src/main/java/com/kiran/controller/AuthController.java
+++ OnlineFoodOrdering/src/main/java/com/kiran/controller/AuthController.java
@@ -10,16 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private AuthService authService;
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "alive";
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> createUser(@RequestBody UserCreateReqDto user) throws BadRequestException {
